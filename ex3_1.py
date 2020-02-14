cbc07a0aa8daed7f89f30d6a37353201d4623219
BH909303 <61058580+BH909303@users.noreply.github.com>
2020-02-14 19:13:57 +0000

    Add files via upload

---
+++ ex3_1.py
@@ -0,0 +1,21 @@
+'''
+py4e, Chapter 3, Exercise 1: Rewrite your pay computation to give the employee
+1.5 times the hourly rate for hours worked above 40 hours
+
+    Enter Hours: 45
+    Enter Rate: 10
+    Pay: 475.0
+
+William Kerley, 14.02.20
+'''
+
+hrs = input("Enter Hours: ")
+rate = input("Enter Rate: ")
+
+hrs = float(hrs)
+rate = float(rate)
+
+if hrs < 40:
+    print("Pay:", hrs*rate)
+else:
+    print("Pay:", 40*rate + (hrs-40)*rate*1.5)
---
+++ ex3_2.py
@@ -0,0 +1,29 @@
+'''
+py4e, Chapter 3, Exercise 2: Rewrite your pay program using 'try' and 'except'
+so that your program handles non-numeric input gracefully by printing a message
+and exiting the program. The following shows two executions of the program:
+
+    Enter Hours: 20
+    Enter Rate: nine
+    Error, please enter numeric input
+
+    Enter Hours: forty
+    Error, please enter numeric input
+
+William Kerley, 14.02.20
+'''
+
+hrs = input("Enter Hours: ")
+try:
+    hrs = float(hrs)
+    rate = input("Enter Rate: ")
+    try:
+        rate = float(rate)
+        if hrs < 40:
+            print("Pay:", hrs*rate)
+        else:
+            print("Pay:", 40*rate + (hrs-40)*rate*1.5)
+    except:
+        print("Error, please enter numeric input")
+except:
+    print("Error, please enter numeric input")
