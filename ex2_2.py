2a9fee90547ef057f6da0757087e2ded0d09741e
BH909303 <61058580+BH909303@users.noreply.github.com>
2020-02-14 18:35:15 +0000

    Add files via upload

---
+++ ex2_2.py
@@ -0,0 +1,9 @@
+'''
+PY4E, Chapter 2, Exercise 2: Write a program that uses 'input' to prompt a user
+for their name and then welcomes them.
+
+William Kerley, 10.02.20
+'''
+
+name = input("Enter your name: ")
+print("Hello", name)
